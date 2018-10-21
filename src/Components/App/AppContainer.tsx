7e33ec55d2e7fe853f7d1411a6d84d1886f99d32
lopun <ujin43255252@gmail.com>
2018-10-21 18:39:37 +0900

    Phone Login verify(react-toastify) added

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -3,11 +3,19 @@ import AppPresenter from "./AppPresenter";
 import { graphql } from "react-apollo";
 import { IS_LOGGED_IN } from "./AppQueries";
 import { ThemeProvider } from "src/typed-components";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 import theme from "src/theme";
 
 const AppContainer = ({ data }) => (
   <ThemeProvider theme={theme}>
-    <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
+    <>
+      <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
+      <ToastContainer
+        draggable={true}
+        position={toast.POSITION.BOTTOM_CENTER}
+      />
+    </>
   </ThemeProvider>
 );
 
--- src/Routes/PhoneLogin/PhoneLoginContainer.tsx
+++ src/Routes/PhoneLogin/PhoneLoginContainer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PhoneLoginPresenter from "./PhoneLoginPresenter";
 import { RouteComponentProps } from "react-router-dom";
+import { toast } from "react-toastify";
 
 interface IState {
   countryCode: string;
@@ -18,13 +19,40 @@ class PhoneLoginContainer extends React.Component<
 
   public render() {
     const { countryCode, phoneNumber } = this.state;
+    const { onInputChange, onSubmit } = this;
     return (
       <PhoneLoginPresenter
         countryCode={countryCode}
         phoneNumber={phoneNumber}
+        onInputChange={onInputChange}
+        onSubmit={onSubmit}
       />
     );
   }
+
+  public onInputChange: React.ChangeEventHandler<
+    HTMLInputElement | HTMLSelectElement
+  > = event => {
+    const {
+      target: { value, name }
+    } = event;
+    this.setState({
+      [name]: value
+    } as any);
+  };
+
+  public onSubmit: React.FormEventHandler<HTMLFormElement> = event => {
+    event.preventDefault();
+    const { countryCode, phoneNumber } = this.state;
+    const isValid = /^\+[1-9]{1}[0-9]{7,11}$/.test(
+      `${countryCode}${phoneNumber}`
+    );
+    if (isValid) {
+      return;
+    } else {
+      toast.error("Please write a valid phone number");
+    }
+  };
 }
 
 export default PhoneLoginContainer;
