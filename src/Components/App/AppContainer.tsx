20e4feb6a4541035a5992ced6faeaa81feebc930
lopun <ujin43255252@gmail.com>
2018-10-21 12:31:46 +0900

    styled-components settings added

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import AppPresenter from "./AppPresenter";
 import { graphql } from "react-apollo";
 import { IS_LOGGED_IN } from "./AppQueries";
+import { ThemeProvider } from "src/typed-components";
+import theme from "src/theme";
 
 const AppContainer = ({ data }) => (
-  <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
+  <ThemeProvider theme={theme}>
+    <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
+  </ThemeProvider>
 );
 
 export default graphql(IS_LOGGED_IN)(AppContainer);
